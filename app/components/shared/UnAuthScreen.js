8e26206cf3da05bbf6a58df00e79a2944c2a99ec
Mukesh Kumar <contactmukesh01@gmail.com>
2018-08-12 10:04:30 +0530

    update content

--- app/components/shared/UnAuthScreen.js
+++ app/components/shared/UnAuthScreen.js
@@ -4,7 +4,8 @@ import {Link} from 'react-router-dom';
 const UnAuthScreen = (props) =>   (
         <div className="unauth-section">
             
-                <h1>You are not Authirised to access this page. </h1>
+                <h1>Not Authorized. </h1>
+                <p>Sorry, you have no permission to see this page.</p>
                 <br />
                 <Link to="/login" >Login Again</Link>
             
